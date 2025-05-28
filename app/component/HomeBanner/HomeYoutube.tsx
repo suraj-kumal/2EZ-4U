91e529f05a8e65695790037b1973d95b943921cf
Suraj Kumal <zerocoolofficial45@gmail.com>
2025-05-28 16:39:25 +0545

    youtube-banner

--- app/component/HomeBanner/HomeYoutube.tsx
+++ app/component/HomeBanner/HomeYoutube.tsx
@@ -9,21 +9,20 @@ const HomeYoutube = () => {
       <h1 className="text-2xl font-bold text-center mb-8 underline underline-offset-4">
         Playlists
       </h1>
-      <div className="flex flex-col items-center justify-center gap-2 mx-4 my-4 lg:grid lg:grid-cols-4">
+
+      <div className="flex flex-wrap justify-center gap-6">
         {playlists.map((playlist, index) => (
           <Link href={playlist.url} key={index} target="_blank">
-            <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-80 flex items-center justify-center">
-              <div className="c h-fit w-fit">
-                <img
-                  className="w-56 h-28 object-cover mx-4 my-4 rounded-xl"
-                  src={`https://img.youtube.com/vi/${playlist.videoId}/0.jpg`}
-                  alt={playlist.title}
-                />
-                <div className="p-4">
-                  <h2 className="font-semibold text-gray-800 mb-2 text-sm text-center">
-                    {playlist.title}
-                  </h2>
-                </div>
+            <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-80 flex flex-col items-center justify-center">
+              <img
+                className="w-56 h-28 object-cover mx-4 my-4 rounded-xl"
+                src={`https://img.youtube.com/vi/${playlist.videoId}/0.jpg`}
+                alt={playlist.title}
+              />
+              <div className="p-4">
+                <h2 className="font-semibold text-gray-800 mb-2 text-sm text-center">
+                  {playlist.title}
+                </h2>
               </div>
             </div>
           </Link>
